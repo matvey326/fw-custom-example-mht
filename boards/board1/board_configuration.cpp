3a870e38efd3e7c3be0a85305c6d2891d26e3b01
matvey326 <zurakovskijmatvej65@gmail.com>
2026-07-02 07:59:47 +1100

    Update board_configuration.cpp

--- boards/board1/board_configuration.cpp
+++ boards/board1/board_configuration.cpp
@@ -1,19 +1,17 @@
 #define EFI_TACTICAL_TRANSMISSION true
+#define EFI_TACTICAL_KNOCK true
+
 #include "pch.h"
 #include "board_overrides.h"
 
-Gpio getCommsLedPin() {
-	return Gpio::Unassigned;
-}
-
-Gpio getRunningLedPin() {
-	return Gpio::Unassigned;
-}
-
-Gpio getWarningLedPin() {
-	return Gpio::Unassigned;
-}
+Gpio getCommsLedPin() { return Gpio::Unassigned; }
+Gpio getRunningLedPin() { return Gpio::Unassigned; }
+Gpio getWarningLedPin() { return Gpio::Unassigned; }
 
 void setup_custom_board_overrides() {
-engineConfiguration->transmissionControllerMode = TransmissionControllerMode::Ford4R70W;
+    // 1. Включаем логику TCU (управление соленоидами АКПП)
+    engineConfiguration->transmissionControllerMode = TransmissionControllerMode::Ford4R70W;
+
+    // 2. Активируем контроль детонации (Knock Sensing)
+    engineConfiguration->isKnockSensingEnabled = true;
 }
